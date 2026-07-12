846f39733db1f522882595247e71d8639fa0b669
randomusert <eetupajamaki@gmail.com>
2026-07-12 13:44:15 +0300

    Prep for content

--- src/main/java/io/github/randomusert/mods/tincraftclassic/Tincraftclassic.java
+++ src/main/java/io/github/randomusert/mods/tincraftclassic/Tincraftclassic.java
@@ -1,12 +1,13 @@
-package com.example.modid;
+package io.github.randomusert.mods.tincraftclassic;
 
+import io.github.randomusert.mods.tincraftclassic.init.*;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 @Mod(modid = Tags.MOD_ID, name = Tags.MOD_NAME, version = Tags.VERSION)
-public class ExampleMod {
+public class Tincraftclassic {
 
     public static final Logger LOGGER = LogManager.getLogger(Tags.MOD_NAME);
 
@@ -18,6 +19,7 @@ public class ExampleMod {
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         LOGGER.info("Hello From {}!", Tags.MOD_NAME);
+        ModInit.ModInit();
     }
 
 }
