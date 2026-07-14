8cda31c5048052dcc52d6569925434009e647a3e
randomusert <eetupajamaki@gmail.com>
2026-07-14 19:15:06 +0300

    Smelting recipe configuration

--- src/main/java/io/github/randomusert/mods/tincraftclassic/config/Config.java
+++ src/main/java/io/github/randomusert/mods/tincraftclassic/config/Config.java
@@ -1,7 +1,8 @@
 package io.github.randomusert.mods.tincraftclassic.config;
 
 
-// All config options defined here and default values provided
+// All config options defined here and default values provided.
+// All values defined here are always the default config.
 public class Config {
     // World gen for ores
     public WorldGeneration worldGeneration = new WorldGeneration();
@@ -12,5 +13,11 @@ public class Config {
         public int tinOreVeinSize = 15;
 
         public int tinOreVeinsPerChunk = 10;
+    }
+
+    public Recipes recipes = new Recipes();
+
+    public static class Recipes {
+        public boolean smeltTinOreToTinIngot = true;
     }
 }
--- src/main/java/io/github/randomusert/mods/tincraftclassic/util/SmeltingRecipes.java
+++ src/main/java/io/github/randomusert/mods/tincraftclassic/util/SmeltingRecipes.java
@@ -1,5 +1,6 @@
 package io.github.randomusert.mods.tincraftclassic.util;
 
+import io.github.randomusert.mods.tincraftclassic.config.ConfigManager;
 import io.github.randomusert.mods.tincraftclassic.init.ModBlocks;
 import io.github.randomusert.mods.tincraftclassic.init.ModItems;
 import net.minecraft.item.ItemStack;
@@ -7,6 +8,8 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class SmeltingRecipes {
     public static void registerSmelting() {
-        GameRegistry.addSmelting(new ItemStack(ModBlocks.TIN_ORE, 1, 0), new ItemStack(ModItems.TIN_INGOT, 1), 1.0f);
+        if (ConfigManager.getConfig().recipes.smeltTinOreToTinIngot) {
+            GameRegistry.addSmelting(new ItemStack(ModBlocks.TIN_ORE, 1, 0), new ItemStack(ModItems.TIN_INGOT, 1), 1.0f);
+        }
     }
 }
